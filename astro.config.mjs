205b127130fb9d9ff3b13778f4f66f3f0fe799d8
Paul Vudmaska <paul@vudmaska.com>
2024-03-14 12:21:54 -0500

    Update function to handle deploying

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://voodoo.gihub.io/blog',
+	base: '/blog/',
 	integrations: [mdx(), sitemap()],
 });
